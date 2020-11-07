9014713265725016bae7099f252ec8e18ff6912d
Shreyan Lowalekar <shreyan.lowalekar@gmail.com>
2020-11-07 17:37:51 +0530

    After Class.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,36 @@
-
+var r1, r2;
 
 function setup()
 {
     createCanvas(800, 400);
 
-    createSprite(400, 200, 50, 50);
+    r1 = createSprite(400, 200, 80, 30);
+    r2 = createSprite(400, 200, 50, 80);
+    r1.shapeColor = "green";
+    r2.shapeColor = "green";
+    r1.debug = true;
+    r2.debug = true;
 }
 
 function draw()
 {
-    //background(255, 255, 255);
-    background(255, 0, 0);
+    background(255, 255, 255);
+    r1.x = World.mouseX;
+    r1.y = World.mouseY;
 
+    if (r1.x - r2.x < r1.width / 2 + r2.width / 2
+        && r2.x - r1.x < r1.width / 2 + r2.width / 2
+        && r1.y - r2.y < r1.height / 2 + r2.height / 2
+        && r2.y - r1.y < r1.height / 2 + r2.height / 2)
+    {
+        r1.shapeColor = "red";
+        r2.shapeColor = "red";
+    }
+    else
+    {
 
+        r1.shapeColor = "green";
+        r2.shapeColor = "green";
+    }
     drawSprites();
 }
